ee9b1cc892b05d958c1aebe7966792153aabcda4
YAMAMOTO Masaya <pandax381@gmail.com>
2019-12-11 14:59:32 +0900

    Improve checksum calculation function

--- pkg/net/util.go
+++ pkg/net/util.go
@@ -4,12 +4,15 @@ func Cksum16(b []byte, n int, init uint32) uint16 {
 	sum := init
 	for i := 0; i < n-1; i += 2 {
 		sum += uint32(b[i])<<8 | uint32(b[i+1])
+		if (sum >> 16) > 0 {
+			sum = (sum & 0xffff) + (sum >> 16)
+		}
 	}
 	if n&1 != 0 {
 		sum += uint32(b[n-1]) << 8
-	}
-	for (sum >> 16) > 0 {
-		sum = (sum & 0xffff) + (sum >> 16)
+		if (sum >> 16) > 0 {
+			sum = (sum & 0xffff) + (sum >> 16)
+		}
 	}
 	return ^(uint16(sum))
 }
